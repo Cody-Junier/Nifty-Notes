68972f4b6797f280ef612fb4998366eadbc69d97
cody-junier <codyjunier@gmail.com>
2021-12-24 12:42:08 -0800

    Fixed catch-all route

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/api/notes', (req, res) =>{
 })
 
 app.get('*', (req, res) =>{
-    res.sendFile(path.join('./public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
 

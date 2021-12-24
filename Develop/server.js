7588bdf111189257c53c5185cd8e2077cd62c2d3
cody-junier <codyjunier@gmail.com>
2021-12-24 12:32:15 -0800

    Initial paths working

--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,27 @@
 const express = require('express');
-
+const fs = require('fs');
+const path = require('path');
 const PORT = process.env.PORT || 3002;
 const app = express();
-const apiRoutes = require('./db/routes/apiRoutes');
-const htmlRoutes = require('./db/routes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static())
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
+app.get('/api/notes', (req, res) =>{
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        const noteList = JSON.parse(data)
+        res.json({noteList})
+    })
+})
+
+app.get('*', (req, res) =>{
+    res.sendFile(path.join('./public/index.html'))
+})
+
 
 app.listen(PORT, ()=>{
     console.log(`Nifty-Notes server now on port ${PORT}`);
